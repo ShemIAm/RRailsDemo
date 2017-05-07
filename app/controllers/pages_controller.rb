75a0a01e157c9357eb857afed001ad5954b1e304
Ishema Mwunvaneza <shemm.i.am@gmail.com>
2017-05-07 07:10:45 +0000

    added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+  # get request for / which is our home page
   def home
+    @fan_plan = Plan.find(1)
+    @supporter_plan = Plan.find(2)
   end
   
   def about
