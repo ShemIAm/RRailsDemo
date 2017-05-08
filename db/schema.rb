c58432c68f29f2614efa6c44178b8ece512cd666
Ishema Mwunvaneza <shemm.i.am@gmail.com>
2017-05-08 08:23:33 +0000

    added stripe token to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506083727) do
+ActiveRecord::Schema.define(version: 20170508082110) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
